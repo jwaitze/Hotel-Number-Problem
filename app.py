4c9a4ff6268bb0bdf7876d4d6c7b1fee1fbcd63f
Jake Waitze <jake@waitze.net>
2017-02-11 04:47:16 -0500

    Added comments

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 import itertools
 
+# naive divisors generation approach
 def get_divisors(n):
     for i in range(1, n+1):
-        if n % i == 0 and i != n:
+        if n % i == 0 and i != n: # don't include the number itself for this
             yield i
 
+# 100 room numbers max
 for i in range(1, 100):
     d = list(get_divisors(i))
     if sum(d) > i:
         found_sum = False
-        for a in range(1, len(d)):
+        for a in range(1, len(d)): # generate permutations of each subset
             for p in itertools.permutations(d, a):
                 if sum(p) == i:
                     found_sum = True
